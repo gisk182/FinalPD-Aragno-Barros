8bbd0cb819b68f9bdff4ec0e22301d598d580975
Barros Matias <matyaz182@gmail.com>
2021-02-17 16:01:11 -0300

    final v1.0

---
+++ Data.hs
@@ -0,0 +1,29 @@
+
+module Data (
+       grid,
+       languages) where
+grid = [ "JFCEHDNAYERRCBD"
+       , "CZSIBEUAMAGFUNM"
+       , "SSHASKELLJOREBH"
+       , "UTAQWABYTGDSVXY"
+       , "ARRLKJBFGHCHULA"
+       , "AAPHPMNBSHRTYUI"
+       , "QWEASGLREPCVBNN"
+       , "ASDXITRMWYSWLPO"
+       , "ZXCBLIEUJTSOMNJ"
+       , "DFGHJRTYUHBZAXS"
+       , "ERFGHNYUJOLKJHG"
+       , "IUHGEDFGCNIJNYG"
+       ]
+
+languages = [ "BASIC"
+            , "COBOL"
+            , "CSHARP"
+            , "HASKELL"
+            , "LISP"
+            , "PERL"
+            , "PHP"
+            , "PYTHON"
+            , "RUBY"
+            , "SCHEME"
+            ]
